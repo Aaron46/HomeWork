50e80268c422e57605a8313e294b3ed81c03a58c
Aaron46 <Aarones2015@gmail.com>
2017-09-14 21:22:51 -0400

    hw-74

---
+++ hw-74/alert.js
@@ -0,0 +1,11 @@
+window.alert(name);
+function greaterThan(x,y){
+    if(x > y){
+        return true;
+    }else{
+        return false;
+    }
+};
+console.log(greaterThan(5,6));
+console.log(greaterThan(10,6));
+console.log(greaterThan(55,102));
